91e6f8f5dece754fc956bd2ec5867f878c21ae7c
Radamés <radames.idiomas@gmail.com>
2024-10-25 00:43:42 -0300

    Change the directory creation script to correctly enumerates the folders.

--- script.py
+++ script.py
@@ -3,8 +3,12 @@ import os
 base_folder = 'day'
 
 for i in range(1, 31):
-    folder_name = f"{base_folder}{i}"
-    os.mkdir(folder_name)
+    if i > 0 and i < 10:
+        folder_name = f"{base_folder}{'0'}{i}"
+        os.mkdir(folder_name)
+    else:
+        folder_name = f"{base_folder}{i}"
+        os.mkdir(folder_name) 
 
     with open(os.path.join(folder_name, '.gitkeep'), 'w') as f:
         pass
